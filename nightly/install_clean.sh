7b0c3a8f588564765890ad84b3ace6260166f02c
Camil Staps <info@camilstaps.nl>
2018-08-29 17:20:21 +0200

    Robustness with nightlies

--- nightly/install_clean.sh
+++ nightly/install_clean.sh
@@ -19,19 +19,18 @@ rm -rf /var/lib/apt/lists/*
 cd /tmp
 git clone https://gitlab.science.ru.nl/clean-and-itasks/clean-build
 sed -i 's/http.*/https:\/\/ftp.cs.ru.nl\/Clean\/builds\/linux-x64\/clean-classic-linux-x64-latest.tgz/' clean-build/clean-base/linux-x64/setup.sh
+sed -i 's/git checkout\(.*\)/(cd src\/$repo; git checkout\1)/' clean-build/generic/fetch.sh
+sed -i 's/\.\/build\.sh/.\/build.sh || true/' clean-build/generic/setup.sh
 
 for TARGET in $TARGETS
 do
-	cd "clean-build/clean-$TARGET/linux-x64"
+	(cd "clean-build/clean-$TARGET/linux-x64"
 
-	./fetch.sh
-	./setup.sh
-	./build.sh
+		./fetch.sh
+		./setup.sh
+		./build.sh
 
-	rsync -r target/clean-$TARGET/* /opt/clean
-
-	cd ../../..
+		rsync -r target/clean-$TARGET/* /opt/clean)
 done
 
-cd /tmp
 rm -rf clean-build
